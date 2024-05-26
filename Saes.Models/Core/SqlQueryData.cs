c41461162d5c348267a6826315401bd165b0ea32
Artem Momatov <momatovv@list.ru>
2024-05-26 17:50:51 +0300

    Add new class to store intermediate data proc calls

---
+++ Saes.Models/Core/SqlQueryData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Saes.Models.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saes.Models.Core
+{
+    public class SqlQueryData
+    {
+        public SqlQueryData(string sql, SqlParameter[] sqlParameters)
+        {
+            Sql = sql;
+            SqlParameters = sqlParameters;
+        }
+
+        public string Sql { get; set; }
+        public SqlParameter[] SqlParameters { get; set; }    
+    }
+}
