1e59b0e699db744295de4d68d27c234676a3c52c
Artem Momatov <momatovv@list.ru>
2024-05-29 23:46:45 +0300

    Minor changes

--- Saes.ConsoleTest/Program.cs
+++ Saes.ConsoleTest/Program.cs
@@ -1,33 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
 using Microsoft.EntityFrameworkCore;
 using Saes.Models;
 using Saes.Models.Schemas;
+using Saes.Protos.ModelServices;
 
-Console.WriteLine("Hello, World!");
 
-using(var ctx = new SaesContext())
+var channel = GrpcChannel.ForAddress("https://localhost:7231");
+
+var userService = new UserService.UserServiceClient(channel);
+
+var lookup = new UserLookup
 {
-    var user  = await ctx.Users.FirstOrDefaultAsync(x => x.Login == "admin");
-    while(true)
+    Login = "admin"
+};
+
+try
+{
+    var response = await userService.SearchAsync(lookup);
+
+    Console.WriteLine("Response data:");
+    for (int i = 0; i < response.Data.Count; i++)
     {
-        Console.Write("Enter password: ");
-        var password = Console.ReadLine();
-
-        if(password == "q")
-        {
-            break;
-        }
-
-        byte[] hash = ctx.udfHashSalt(password, user.PasswordSalt);
-        if(hash.SequenceEqual(user.PasswordHash))
-        {
-            Console.WriteLine("Доступ есть");
-            Console.ReadLine();
-
-        }
-        else
-        {
-            Console.WriteLine("Неверный пароль");
-        }
+        Console.WriteLine( $"\t User Id {response.Data[i].UserId}" );
     }
 }
+catch (Exception ex)
+{
+
+	throw;
+}
+
+Console.ReadLine();
---
+++ nuger_downgrade.ps1
@@ -0,0 +1,13 @@
+Uninstall-Package Avalonia.Themes.Fluent
+Uninstall-Package Avalonia.ReactiveUI
+Uninstall-Package Avalonia.Fonts.Inter
+Uninstall-Package Avalonia.Diagnostics
+Uninstall-Package Avalonia
+
+$version = "11.0.5"
+
+Install-Package Avalonia.Themes.Fluent	-Version $version
+Install-Package Avalonia.ReactiveUI		-Version $version
+Install-Package Avalonia.Fonts.Inter	-Version $version
+Install-Package Avalonia.Diagnostics	-Version $version
+Install-Package Avalonia				-Version $version
