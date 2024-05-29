2d993c08fc9f2f263dabcaea437165dada835fab
Artem Momatov <momatovv@list.ru>
2024-05-29 23:46:15 +0300

    Removed useless UserService

--- Saes.GrpcServer/Services/Interfaces/IUserService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Saes.GrpcServer.Services.Interfaces
-{
-    public interface IUserService
-    {
-        public bool VerifyUser(string username, string password);
-        public Task<bool> VerifyUserAsync(string username, string password);
-    }
-}
---
+++ Saes.GrpcServer/Services/ServicesRegister.cs
@@ -0,0 +1,14 @@
+﻿using Saes.GrpcServer.Services.Implementations;
+using Saes.GrpcServer.Services.Interfaces;
+
+namespace Saes.GrpcServer.Services
+{
+    public class ServicesRegister
+    {
+        public void Register(IServiceCollection services)
+        {
+            services.AddScoped<ILogAuthenticationService, LogAuthenticationService>();
+            services.AddSingleton<ITokenService, TokenService>();
+        }
+    }
+}
