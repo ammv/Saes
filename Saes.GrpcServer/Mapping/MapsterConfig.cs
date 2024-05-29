7f8a135b2ac36b065c627c5065fcec8685d08bd9
Artem Momatov <momatovv@list.ru>
2024-05-29 23:45:13 +0300

    Added mapping for DTO

---
+++ Saes.GrpcServer/Mapping/MapsterConfig.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using Saes.Models;
+using Saes.Protos;
+
+namespace Saes.GrpcServer.Mapping
+{
+    public class MapsterConfig
+    {
+        public MapsterConfig()
+        {
+            //TypeAdapterConfig<UserDto, User>.NewConfig()
+            //    .Map(dest => dest.UserId, src => src.Id)
+            //    .Map(dest => dest.UserName, src => src.Name)
+            //    .Map(dest => dest.UserEmail, src => src.Email);
+        }
+    }
+}
---
+++ Saes.GrpcServer/Mapping/RegisterMapper.cs
@@ -0,0 +1,25 @@
+﻿using Mapster;
+using Saes.Models;
+using Saes.Protos;
+
+namespace Saes.GrpcServer.Mapping
+{
+    public class RegisterMapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Models.File, FileDto>()
+                .RequireDestinationMemberSource(true);
+            config.NewConfig<UserRole, UserRoleDto>()
+                .RequireDestinationMemberSource(true);
+            config.NewConfig<RightGroup, RightGroupDto>()
+                .RequireDestinationMemberSource(true);
+            config.NewConfig<Right, RightDto>()
+                .RequireDestinationMemberSource(true);
+            config.NewConfig<UserRoleRight, UserRoleRightDto>()
+                .RequireDestinationMemberSource(true);
+            config.NewConfig<User, UserDto>()
+                .RequireDestinationMemberSource(true);
+        }
+    }
+}
