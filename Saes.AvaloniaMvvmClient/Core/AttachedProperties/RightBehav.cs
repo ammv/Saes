4abff47078b668a96684758c204aca200ea9cec5
Artem Momatov <momatovv@list.ru>
2024-06-13 04:58:09 +0300

    Added new AttachedProperty for right system

---
+++ Saes.AvaloniaMvvmClient/Core/AttachedProperties/RightBehav.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Saes.AvaloniaMvvmClient.Core.AttachedProperties
+{
+    public class RightBehav: AvaloniaObject
+    {
+        public static readonly AttachedProperty<string> RightCodeProperty = AvaloniaProperty.RegisterAttached<RightBehav, string>(
+            "RightCode", typeof(string), default);
+
+        /// <summary>
+        /// Accessor for Attached property <see cref="CommandProperty"/>.
+        /// </summary>
+        public static void SetRightCode(AvaloniaObject element, string rightCode)
+        {
+            element.SetValue(RightCodeProperty, rightCode);
+        }
+
+        /// <summary>
+        /// Accessor for Attached property <see cref="CommandProperty"/>.
+        /// </summary>
+        public static string GetRightCode(AvaloniaObject element)
+        {
+            return element.GetValue(RightCodeProperty);
+        }
+
+    }
+}
